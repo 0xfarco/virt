59398470723dbf80515816f42b89feb166c74546
0xfarco <0xfarco@gmail.com>
2024-12-11 22:29:42 +0000

    ...

--- core.sh
+++ core.sh
@@ -2,16 +2,13 @@
 
 set -xe
 
-ISO_PATH="$1"
-QCOW2_IMAGE="$2"
-SIZE="$3"
+QCOW2_IMAGE="$1"
 
 # TODO: check if the user didn't provide enough arguments
 
-qemu-img create -f qcow2 "$QCOW2_IMAGE" "$SIZE"
 qemu-system-x86_64 -enable-kvm \
                    -m 2G \
-                   -cdrom "$ISO_PATH" \
+                   -smp 4 \
+                   -cpu host \
                    -hda "$QCOW2_IMAGE" \
-                   -boot d \
                    -vga virtio -display sdl,gl=on
